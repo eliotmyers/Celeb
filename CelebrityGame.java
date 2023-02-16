c5c4251559adc959dc14d1ad46d25c8b64ba77eb
unknown <eliot.myers@salesianum.org>
2023-02-15 23:42:13 -0500

    submit

--- CelebrityGame.java
+++ CelebrityGame.java
@@ -69,30 +69,7 @@ public class CelebrityGame {
     }
 }
 
-class Celebrity {
-    private String name;
-    private List<String> traits;
 
-    public Celebrity(String name, String... traits) {
-        this.name = name;
-        this.traits = new ArrayList<>();
-        for (String trait : traits) {
-            this.traits.add(trait);
-        }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public List<String> getTraits() {
-        return traits;
-    }
-
-    public boolean guessTrait(String guess, int index) {
-        return guess.equalsIgnoreCase(traits.get(index));
-    }
-}
 
     
         
---
+++ traits.java
@@ -0,0 +1,7 @@
+public class traits {
+    
+}
+
+public class traits {
+
+}
